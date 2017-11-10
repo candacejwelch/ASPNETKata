a92d71fe5ff3a743948a54575569f6e934cae838
Candace <candacejwelch@gmail.com>
2017-11-10 13:22:40 -0600

    refactored

--- ASPNETKata/Controllers/ProductController.cs
+++ ASPNETKata/Controllers/ProductController.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using ASPNETKata.Models;
-using MySql.Data.MySqlClient;
+﻿using System.Web.Mvc;
 using Microsoft.Practices.Unity;
 using SqlIntro;
 
@@ -23,13 +14,6 @@ namespace ASPNETKata.Controllers
         // GET: Product
         public ActionResult Index()
         {   
-            //var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
-            //using (var conn = new MySqlConnection(connectionString))
-            //{
-            //    conn.Open();
-            //    var list = conn.Query<Product>("select * from product ORDER BY ProductId DESC");
-            //    return View(list);
-            //}
             var list = ProductRepository.GetProducts();
             return View(list);
         }
@@ -37,23 +21,8 @@ namespace ASPNETKata.Controllers
         //GET: Product/Details/5
         public ActionResult Details(int id)
         {
-            
-
-            //var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
-            //using (var conn = new MySqlConnection(connectionString))
-            //{
-            //    conn.Open();
-            //    try
-            //    {
-            //        conn.Execute("select * product where ProductID = @id", id);
-            //        return RedirectToAction("Index");
-            //    }
-            //    catch
-            //    {
             var list = ProductRepository.GetDetails(id);
             return View(list);
-            //    }
-            //}
         }
 
         // GET: Product/Create
@@ -66,38 +35,8 @@ namespace ASPNETKata.Controllers
         [HttpPost]
         public ActionResult Create(Product product)
         {
-            //var prod = new Product
-            //{
-            //    Name = collection["Name"],
-            //    //ProductId = int.Parse(collection["ProductId"])
-            //};
-            
-            //var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
-            //using (var conn = new MySqlConnection(connectionString))
-            //{
-            //    conn.Open();
-            //    try
-            //    {
-            //        conn.Execute("INSERT into product (Name) values (@Name)", prod);
-            //        return RedirectToAction("Index");
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Console.WriteLine(e);
-
-            try
-            {
-                ProductRepository.InsertProduct(product);
-                return RedirectToAction("Index");
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-            
-            //    }
-            //}
-
+            ProductRepository.InsertProduct(product);
+            return RedirectToAction("Index");
         }
 
         // GET: Product/Edit/5
@@ -113,42 +52,13 @@ namespace ASPNETKata.Controllers
         {
 
             product.ProductId = id;
-
-            //var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
-            //using (var conn = new MySqlConnection(connectionString))
-            //{
-            //    conn.Open();
-            //    try
-            //    {
-            //        conn.Execute("update product set name = @name where ProductID = @id",
-            //            new {id, name});
-            //        return RedirectToAction("Index");
-            //    }
-            //    catch
-            //    {
-            try
-            {
-                ProductRepository.UpdateProduct(product);
-                return RedirectToAction("Index");
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-            //    }
-            //}
+            ProductRepository.UpdateProduct(product);
+            return RedirectToAction("Index");
         }
 
         // GET: Product/Delete/5
         public ActionResult Delete(int id)
         {
-            
-            //var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
-            //using (var conn = new MySqlConnection(connectionString))
-            //{
-            //    conn.Query("select (Name, ProductId) from product where ProductID = @id")
-            //}
-            
             return View();
         }
 
@@ -156,27 +66,8 @@ namespace ASPNETKata.Controllers
         [HttpPost]
         public ActionResult Delete(int id, Product product)
         {
-            //var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
-            //using (var conn = new MySqlConnection(connectionString))
-            //{
-            //    conn.Open();
-            //    try
-            //    {
-            //        conn.Execute("delete from product where ProductID = @id", new { id = productId });
-            //        return RedirectToAction("Index");
-            //    }
-            //    catch
-            //    {
-            try
-            {
-                ProductRepository.DeleteProduct(id);
-                return RedirectToAction("Index");
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }            //    }
-            //}
+            ProductRepository.DeleteProduct(id);
+            return RedirectToAction("Index");
         }
     }
 }
